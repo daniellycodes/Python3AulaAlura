6feab98ff88a87af1917c3a66708dfc6e324ce90
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-06-06 08:11:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello, World!')
+print('********************************')
+print('Bem-vindo ao jogo de Adivinhação')
+print('********************************')
+
+numero_secreto = 42
+
+chute = input('Digite o seu numero:')
+
+print('Voce digitou', chute)
+
+if(numero_secreto==chute):
+  print('Voce acertou!')
+else: 
+  print('Voce errou!')
