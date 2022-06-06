b325a8bfc0b0dc9c367b17523fec11e89c2af8f8
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-06-06 08:18:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ print('********************************')
 
 numero_secreto = 42
 
-chute = input('Digite o seu numero:')
+chute_str = input('Digite o seu numero:')
 
-print('Voce digitou', chute)
+print('Voce digitou', chute_str)
+
+chute = int(chute_str)
 
 if(numero_secreto==chute):
   print('Voce acertou!')
 else: 
   print('Voce errou!')
+  
+  print('Fim do jogo')
