6f367917f6762ce5a803021891a3aeb58083a267
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-06-06 09:01:39 -0300

    Update and rename main.py to adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,9 +10,14 @@ print('Voce digitou', chute_str)
 
 chute = int(chute_str)
 
-if(numero_secreto==chute):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
   print('Voce acertou!')
 else: 
-  print('Voce errou!')
-  
-  print('Fim do jogo')
+  if(maior):
+    print('Voce errou! O seu chute foi maior do que o numero secreto')
+  elif(menor):
+    print('Voce errou! O seu chute foi menor do que o numero secreto!')
