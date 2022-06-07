59d2596cbcc596fb747d265e77c26345656c31b1
Danielly Silva <98064933+daniellycodes@users.noreply.github.com>
2022-06-07 20:28:24 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,9 +1,10 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
-rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -13,7 +14,7 @@ for rodada in range(1, total_de_tentativas + 1):
     chute = int(chute_str)
 
     if(chute < 1 or chute > 100):
-      print("Você deve digitar um número entre 1 e 100.")
+      print("Você deve digitar um número entre 1 e 100")
       continue
 
     acertou = chute == numero_secreto
@@ -29,4 +30,4 @@ for rodada in range(1, total_de_tentativas + 1):
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
 
-print("Fim do jogo")
+print("Fim do jogo") 
